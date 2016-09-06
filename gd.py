11dce6a8be2385e516ce8357347777cd5f085eb4
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-09-05 18:10:29 -0700

    gradient descent for X and y data implemented

--- gd.py
+++ gd.py
@@ -1,41 +1,44 @@
 """
-Generic implementation of gradient descent.
+Implementation of gradient descent.
 """
-
+import numpy as np
 from numpy import *
-import util
 
-def gd(func, grad, x0, numIter, stepSize):
-    """
-    Perform gradient descent on some function func, where grad(x)
-    computes its gradient at position x.  Begin at position x0 and run
-    for exactly numIter iterations.  Use stepSize/sqrt(t+1) as a
-    step-size, where t is the iteration number.
-    We return the final solution as well as the trajectory of function
-    values.
-    """
-
-    # initialize current location
-    x = x0
+def run_gd(X, y, numIter, stepSize):
+    (h,w) = X.shape
+    theta = np.zeros(w)
 
     # set up storage for trajectory of function values
-    trajectory = zeros(numIter + 1)
-    trajectory[0] = func(x)
+    # trajectory = zeros(numIter + 1)
+    # trajectory[0] = theta
 
     # begin iterations
-    for iter in range(numIter):
+    for i in range(numIter):
+        pred = np.dot(X,theta)
+
+        error = pred - y
+        cost = np.sum(error**2)/(2*y.size)
 
         # compute the gradient at the current location
-        g = grad(x)
+        g = X.T.dot(error)/y.size
 
         # compute the step size
-        eta = stepSize/sqrt(iter+1)
+        # eta = stepSize/sqrt(iter+1)
 
         # step in the direction of the gradient
-        x = x - eta*g
+        theta = theta - stepSize*g
 
         # record the trajectory
-        trajectory[iter+1] = func(x)
+        # trajectory[iter+1] = theta
 
     # return the solution
-return (x, trajectory)
+    return theta
+
+if __name__ == '__main__':
+    X = genfromtxt('Xmatrix.dat')
+    y = genfromtxt('yvector.dat')
+
+    numIter = 100
+    stepSize = 0.01
+    theta = run_gd(X, y, numIter, stepSize)
+    print theta
