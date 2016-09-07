57065c8b6dbb67227958146972af84c4bcd3c224
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-09-06 18:05:18 -0700

    added

--- gd.py
+++ gd.py
@@ -8,37 +8,37 @@ def run_gd(X, y, numIter, stepSize):
     (h,w) = X.shape
     theta = np.zeros(w)
 
-    # set up storage for trajectory of function values
-    # trajectory = zeros(numIter + 1)
-    # trajectory[0] = theta
-
     # begin iterations
     for i in range(numIter):
         pred = np.dot(X,theta)
 
         error = pred - y
-        cost = np.sum(error**2)/(2*y.size)
 
         # compute the gradient at the current location
-        g = X.T.dot(error)/y.size
-
-        # compute the step size
-        # eta = stepSize/sqrt(iter+1)
+        g = np.dot(np.transpose(X),error)/y.size
+        #g = X.T.dot(error)/y.size
 
         # step in the direction of the gradient
         theta = theta - stepSize*g
 
-        # record the trajectory
-        # trajectory[iter+1] = theta
-
     # return the solution
     return theta
+
 
 if __name__ == '__main__':
     X = genfromtxt('Xmatrix.dat')
     y = genfromtxt('yvector.dat')
 
-    numIter = 100
-    stepSize = 0.01
-    theta = run_gd(X, y, numIter, stepSize)
-    print theta
+    #X = np.loadtxt('Xmatrix.dat')
+    #y = np.loadtxt('yvector.dat')
+
+    print X
+    print y
+
+    numIter = 10000
+    stepSize = 0.0
+    while stepSize <= 1.0:
+        theta = run_gd(X, y, numIter, stepSize)
+        print stepSize
+        print np.linalg.norm(theta)
+        stepSize += 0.05
