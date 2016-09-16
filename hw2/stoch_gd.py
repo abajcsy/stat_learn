8485789fbc43d81b8288272b8a7639fd390e0070
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-09-16 00:24:18 -0700

    updates

--- hw2/stoch_gd.py
+++ hw2/stoch_gd.py
@@ -5,29 +5,35 @@ import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt
 
+def log_loss(X,y,theta):
+    sum = 0
+    for i in range(m):
+        sum += log(1+np.exp(-y[i]*((theta.T)*X[i] + b)))
+    sum = -sum
 
 def stoch_gd(X, y, numIter, stepSize, epsilon):
     (m,n) = np.shape(X)
     theta = np.random.rand(n)
 
     # begin iterations
-    for t in range(numIter):
+    for i in range(numIter):
         I = np.random.randint(0,m)
         XIt = np.transpose(X[I])
 
         # compute the gradient at the current location
-        g = -y[I]*XIt + XIt*(exp(XIt*theta)/(1+exp(XIt*theta)))
+        g = log(np.exp(-y[I]*((theta.T)*X[I])))
+        # g = -y[I]*XIt + XIt*(exp(XIt*theta)/(1+exp(XIt*theta)))
 
         # step in the direction of the gradient
-        theta2 = theta - stepSize/(t+1)*g
+        theta2 = theta - stepSize/(i+1)*g
 
         if(np.dot(theta2-theta, theta2-theta) < epsilon):
-            return 1
+            return theta
         else:
             theta = theta2
 
     # return the solution
-    return 0
+    return theta
 
 
 if __name__ == '__main__':
@@ -42,16 +48,17 @@ if __name__ == '__main__':
     j = 1
     result = np.empty([m, 1])
 
-    while i > 0.0000001:
-        stepSize = i/10
-        res = stoch_gd(X, y, numIter, stepSize, epsilon)
-        print res
-        result[j] = res;
-        j += 1
-        i /= 10.0
+    #while i > 0.0000001:
+    stepSize = i/100
+    res = stoch_gd(X, y, numIter, stepSize, epsilon)
+    thetaX = np.transpose(res)*X
+    print thetaX.T*y
+    #result[j] = np.exp(thetaX.T*y)/(1+np.exp(thetaX));
+    #j += 1
+    #i /= 10.0
 
-    plt.plot(result)
-    plt.axis([0, m, 0, 1.5])
+    plt.plot(np.transpose(thetaX)*y)
+    #plt.axis([0, m, 0, 1.5])
     plt.show()
 
     # verify what alpha it converges for
