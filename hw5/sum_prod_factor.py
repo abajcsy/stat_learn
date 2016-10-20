b7d5c8cf574a2558a7414f7e165002101b7df6aa
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-10-19 17:12:07 -0700

    bla

--- hw5/sum_prod_factor.py
+++ hw5/sum_prod_factor.py
@@ -4,6 +4,48 @@ Implementation of problem 4.2
 import numpy as np
 from numpy import *
 
+class Compat():
+	nodes = []
+	table = []
+
+	def __init__(self, nodes, table):
+		self.nodes = nodes
+		self.table = table
+
+	def get_indices_from_nodes(node_list):
+		# if Compat has nodes [1,3], then it has a 2x2 table of compat function
+		# given node_list [1], it returns a list [0] for the index of node [1] values
+
+		# if Compat has nodes [1,4,7], then it has a 2x2x2 table of compat function
+		# given node_list [1,4], it returns a list [0,1] for the index of node [1,4] values		
+		indices = []
+		for i in node_list:
+			
+
+	def multiply(compat):
+		union_nodes = self.nodes
+		# find all common nodes from two compat functions
+		for m in compat.nodes:
+			if m in union_nodes:
+				continue
+			else:
+				union_nodes.append(m)
+
+		dim_tuple = ()
+		for i in len(union_nodes):
+			dim_tuple = dim_tuple + (2,)
+
+		new_table = np.array(dim_tuple)
+		
+		#for each node_s in self.nodes
+			# index into the self.table to get (0,0), (0,1), (1,0), etc. element
+		#for each node_t in compat.nodes
+			# index into compat.table to get ()
+			new_table[]
+				
+	def divide(compat):
+		
+
 class SepSet():
 	nodes = []
 	compat = []
@@ -11,13 +53,19 @@ class SepSet():
 	def __init__(self, nodes):
 		self.nodes = nodes
 
-	# tells you if a given node s is in the separator set
-	def indicator(self, s):
-		if s in nodes:
-			return 1
+		# make separator set compatibility function initialized to unity
+		if len(self.nodes) == 2:
+			compat = np.array((2,2))
+				for i in range(2):
+					for j in range(2):
+						compat[i][j] = 1
 		else:
-			return 0
-
+			compat = np.array((2,2,2))
+			for i in range(2):
+				for j in range(2):
+					for j in range(2):
+						compat[i][j][k] = 1
+					
 # Node class that stores the compatability function at that node, 
 # the list of neighbor nodes it sends/receives messages to/from,
 # the list of incoming messages and outgoing messages
@@ -25,16 +73,14 @@ class Clique():
 	# list of nodes in this clique
 	nodes = []
 	# vector for clique's compatibility function
-	compat = []
+	compat = None
 	# list of neighboring cliques
 	neigh = []
 
-	# these are dictionaries that map a clique's neighbors 
-	# to the message it's sending/receiving  
-	in_msgs = {}
-	out_msgs = {}
-
-	def __init__(self, compat):
+	def __init__(self, nodes, neigh, compat):
+		self.nodes = nodes
+		self.neigh = neigh
+		# make clique compatibility function
 		self.compat = compat
 		
 # Tree class represents a tree as a set of nodes. Each node
@@ -53,89 +99,30 @@ class JunctionTree():
 		# create list of separator sets
 		sep_sets = [SepSet([1,3]), SepSet([3,7]), SepSet([1,4,7]), SepSet([1,5]), SepSet([5,7])]
 
-		# create list of cliques in junction tree
-		for i in range(self.size):
-			self.cliques.append(Clique([]))
- 
-		self.cliques[0].nodes = [0,1,3]
-		self.cliques[0].neigh = [1] # stores indices of neighboring cliques in self.cliques
+		# create list of cliques in junction tree, each with standard compatibility function
+		c01 = Compat([0,1], np.array([1.0, 0.3][0.5, 1.0]))
+		c03 = Compat([0,3], np.array([1.0, 0.3][0.5, 1.0]))
+		c14 = Compat([1,4], np.array([1.0, 0.3][0.5, 1.0]))
+		c34 = Compat([3,4], np.array([1.0, 0.3][0.5, 1.0]))
+		c47 = Compat([4,7], np.array([1.0, 0.3][0.5, 1.0]))
+		c36 = Compat([3,6], np.array([1.0, 0.3][0.5, 1.0]))
+		c67 = Compat([6,7], np.array([1.0, 0.3][0.5, 1.0]))
+		c12 = Compat([1,2], np.array([1.0, 0.3][0.5, 1.0]))
+		c25 = Compat([2,5], np.array([1.0, 0.3][0.5, 1.0]))
+		c45 = Compat([4,5], np.array([1.0, 0.3][0.5, 1.0]))
+		c58 = Compat([5,8], np.array([1.0, 0.3][0.5, 1.0]))
+		c78 = Compat([7,8], np.array([1.0, 0.3][0.5, 1.0]))
 
-		self.cliques[1].nodes = [1,3,4,7]
-		self.cliques[1].neigh = [0,2,4]
+		self.cliques.append(Clique([0,1,3], [1], c01.multiply(c03)))
+		self.cliques.append(Clique([1,3,4,7], [0,2,4], c14.multiply(c34).multiply(c47)))
+		self.cliques.append(Clique([3,6,7], [1], c36.multiply(c67)))
+		self.cliques.append(Clique([1,2,5], [4], c12.multiply(c25)))
+		self.cliques.append(Clique([1,4,5,7], [1,3,5], c45))
+		self.cliques.append(Clique([5,7,8], [4], c58.multiply(c78)))
+
+	def init_sep_set():
 		
-		self.cliques[2].nodes = [3,6,7]
-		self.cliques[2].neigh = [1]
-
-		self.cliques[3].nodes = [1,2,5]
-		self.cliques[3].neigh = [4]
-
-		self.cliques[4].nodes = [1,4,5,7]
-		self.cliques[4].neigh = [1,3,5]
-
-		self.cliques[5].nodes = [5,7,8]
-		self.cliques[5].neigh = [4]
-
-		# initialize messages for all edges uniformly at first
-		# for incoming and outgoing edges
-		for i in range(self.size):
-			self.cliques[i].out_msgs = {}
-			self.cliques[i].in_msgs = {}
-			for j in self.cliques[i].neigh:
-				self.cliques[i].out_msgs[j] = np.array([1,1]) 
-				self.cliques[i].in_msgs[j] = np.array([1,1])
 	
-	# defines the compatibility function for a given edge (or pair of nodes)
-	def edge_compat(self, s, t):
-		if s == t:
-			return 1.0
-		else:
-			if s == 0 and t == 1:
-				return 0.3
-			else:
-				return 0.5	
-
-	# runs sum-product algorithm on tree
-	def sum_prod_factor(self):
-		numIter = 20
-		for k in range(numIter):
-			# for each clique t
-			for s in range(self.size):
-				# for each clique s that has an edge with t
-				for t in self.cliques[s].neigh:	
-					self.cliques[t].out_msgs[s] = np.array([0,0])
-					# compute for x_s = 0 and x_s = 1
-					for idx in range(2):
-						# get edge compatibility function 
-						edge_compat0 = self.edge_compat(idx,0)
-						edge_compat1 = self.edge_compat(idx,1)
-						edge_compat = np.array([edge_compat0, edge_compat1])
-
-						# get t's singleton compatibility function 
-						compat_t = self.cliques[t].compat
-					
-						# compute final product of edge and singleton compatibility function
-						final_compat = compat_t[idx]*edge_compat
-
-						# compute product of all the received messages from neighbors 
-						# that are NOT the one you are sending a message to
-						vec_prod = np.array([1,1])
-						for u in self.cliques[t].neigh:
-							if u != s:
-								vec_prod = vec_prod*self.cliques[u].out_msgs[t][idx]
-										
-						result = final_compat * vec_prod					
-						self.cliques[t].out_msgs[s] =  self.cliques[t].out_msgs[s] + result		
-					self.cliques[s].in_msgs[t] = self.cliques[t].out_msgs[s]
-					
-		# compute marginals from formula:
-		# p(x_s) = psi(x_s)* prod_over_neighbors(M*_(t->s)(x_s))
-		for i in range(self.size):
-			marg = np.prod(self.cliques[i].in_msgs.values(),0)
-			marg = self.cliques[i].compat*marg
-			marg_norm = marg/np.sum(marg)
-			print "p(",i, ") = ", marg_norm
-			
 if __name__ == '__main__':
 	t = JunctionTree()	
-	t.sum_prod_factor()
 
