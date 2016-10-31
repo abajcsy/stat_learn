055033e21a59122166ada3b7d813cb625db51b45
Andrea Bajcsy <andrea.victoria.b@gmail.com>
2016-10-30 21:10:54 -0700

    prob 6

--- hw6/accept_rej.py
+++ hw6/accept_rej.py
@@ -2,27 +2,34 @@ import numpy as np
 from numpy import *
 import matplotlib.pyplot as plt
 
-def p(x,c):
+# distribution of X with density f
+def f(x,c):
 	if x >= 0 and x <= 1:
 		return c*x*(1-x)
 	else:
 		return 0
 
-def q(x):
+# distribution of Y with denisty g
+def g(x):
+	return np.random.uniform(0,1) 
 	
-
 if __name__ == "__main__":
 	N = 1000
-	M = 2 # M > 1 and bound on p(x)/q(x)
-	c = 10
+	M = 2 # M > 1 and bound on f(x)/g(x)
+	c = 6
 	x = [None] * N
 	i = 0
-	while i not N:
-		x[i]  = q(x)
+	while i != N:
+		y  = g(x)
+		# sample u from Unif(0,1)
 		u = np.random.uniform(0,1) 
-		if u < (p(x[i], c) / M*q(x[i])) : 
-			accept x_i
+		if u < (f(y, c) / M*g(y)) : 
+			#accept y as sample drawn from f
+			x[i] = y
 			i = i + 1
-		else:
-			reject x_i
+			print "accept: ", y
+		# else: reject y and return to sampling
 	
+	plt.hist(x,N)
+	plt.title("Accept-Reject Sampling Histogram")
+	plt.show()
